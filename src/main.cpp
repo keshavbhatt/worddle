c94e568456dbb2166401125ffc69363fe66b06c7
Keshav Bhatt <keshavnrj@gmail.com>
2022-02-21 04:25:36 +0530

    chore: HDPI support. other optimizations
    Signed-off-by: Keshav Bhatt <keshavnrj@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,20 @@ int main(int argc, char *argv[]) {
   qputenv("QTWEBENGINE_REMOTE_DEBUGGING", "23654");
 #endif
 
+  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  static const char ENV_VAR_QT_DEVICE_PIXEL_RATIO[] = "QT_DEVICE_PIXEL_RATIO";
+  if (!qEnvironmentVariableIsSet(ENV_VAR_QT_DEVICE_PIXEL_RATIO) &&
+      !qEnvironmentVariableIsSet("QT_AUTO_SCREEN_SCALE_FACTOR") &&
+      !qEnvironmentVariableIsSet("QT_SCALE_FACTOR") &&
+      !qEnvironmentVariableIsSet("QT_SCREEN_SCALE_FACTORS")) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  }
+
   QApplication a(newArgc, newArgv);
 
   QApplication::setApplicationName("Worddle");
   QApplication::setOrganizationName("org.keshavnrj.ubuntu");
   QApplication::setApplicationVersion(VERSIONSTR);
-
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
 // allow multiple instances in debug builds
 #ifndef QT_DEBUG
@@ -39,6 +46,12 @@ int main(int argc, char *argv[]) {
                           "An instance of " + appname + " is already running.");
     return 0;
   }
+#endif
+
+  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--single-process");
+  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--disable-logging");
+#ifdef QT_DEBUG
+  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--remote-debugging-port=9421");
 #endif
 
   MainWindow w;
