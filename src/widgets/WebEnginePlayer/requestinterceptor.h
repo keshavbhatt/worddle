62289570d76b555ad06ccd3cd7b670c1df6cf2b1
Keshav Bhatt <keshavnrj@gmail.com>
2022-02-21 04:24:32 +0530

    feat: add request interceptor
    Signed-off-by: Keshav Bhatt <keshavnrj@gmail.com>

---
+++ src/widgets/WebEnginePlayer/requestinterceptor.h
@@ -0,0 +1,27 @@
+#include <QWebEngineUrlRequestInfo>
+#include <QWebEngineUrlRequestInterceptor>
+
+class RequestInterceptor : public QWebEngineUrlRequestInterceptor {
+  Q_OBJECT
+
+public:
+  RequestInterceptor(QObject *parent = nullptr)
+      : QWebEngineUrlRequestInterceptor(parent) {}
+
+  void interceptRequest(QWebEngineUrlRequestInfo &info) {
+
+    QString reqUrlStr = info.requestUrl().toString();
+
+    QStringList badUrls = {"googletagmanager.com",
+                           "google-analytics",
+                           "dailywordle.com/images/",
+                           "nyt.com",
+                           "doubleclick.net",
+                           "pnytimes.chartbeat.net",
+                           "nytimes.com/track"};
+    foreach (QString badUrl, badUrls) {
+      if (reqUrlStr.contains(badUrl, Qt::CaseInsensitive))
+        info.block(true);
+    }
+  }
+};
