1c5c7a5683d8273c2e0ebd9c8d110b9b5d5b3250
Keshav Bhatt <keshavnrj@gmail.com>
2022-03-05 23:42:27 +0530

    fix qcf

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,6 @@
 #include "rungaurd.h"
 
 int main(int argc, char *argv[]) {
-  /*
-   * Add extra arguments on runtime
-   */
-  char ARG_DISABLE_WEB_SECURITY[] = "--disable-web-security";
-  int newArgc = argc + 1 + 1;
-  char **newArgv = new char *[newArgc];
-  for (int i = 0; i < argc; i++) {
-    newArgv[i] = argv[i];
-  }
-  newArgv[argc] = ARG_DISABLE_WEB_SECURITY;
-  newArgv[argc + 1] = nullptr;
-
-#ifdef QT_DEBUG
-  qputenv("QTWEBENGINE_REMOTE_DEBUGGING", "23654");
-#endif
 
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   static const char ENV_VAR_QT_DEVICE_PIXEL_RATIO[] = "QT_DEVICE_PIXEL_RATIO";
@@ -31,7 +16,13 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   }
 
-  QApplication a(newArgc, newArgv);
+#ifdef QT_DEBUG
+  qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--remote-debugging-port=9421");
+#endif
+  qputenv("QTWEBENGINE_CHROMIUM_FLAGS",
+          "--disable-logging --single-process --disable-web-security");
+
+  QApplication a(argc, argv);
 
   QApplication::setApplicationName("Worddle");
   QApplication::setOrganizationName("org.keshavnrj.ubuntu");
@@ -46,12 +37,6 @@ int main(int argc, char *argv[]) {
                           "An instance of " + appname + " is already running.");
     return 0;
   }
-#endif
-
-  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--single-process");
-  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--disable-logging");
-#ifdef QT_DEBUG
-  qputenv("QTWEBENGINE_CHROMIUM_FLAGS","--remote-debugging-port=9421");
 #endif
 
   MainWindow w;
